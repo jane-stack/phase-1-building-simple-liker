ccb04a9a33a034a826736717b817dae769b211d2
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2022-06-24 17:41:00 -0600

    commit tree 11ff0a25d4d8cdad43012acec0f0a3f6da01cd3d

--- main.js
+++ main.js
@@ -3,9 +3,52 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector("#modal")
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("The DOM has loaded")
+  errorModal.classList.add("hidden")
 
+  clickListener()
+})
 
+function hideError() {
+  errorModal.classList.add("hidden")
+}
+function findLikes() {
+  const likeArr = document.querySelector(".like-glyph")
+  likeArr.forEach((singularLike) => {
+    singularLike.addEventListener("click", () => console.log("YOU FOUND ME! LIKE!"))
+  })
+}
+
+function clickListener() {
+  document.addEventListener("click", (e) => {
+    if (e.target.classList[0] === "like-glyph") {
+      mimicServerCall()
+      .then(resp => {
+        const activated = e.target.classList.contains("activated-heart")
+        if (activated) {
+          e.target.classList.remove("activated-heart")
+          e.target.innerHTML = EMPTY_HEART
+        } else {
+          e.target.classList.add("activated-heart")
+          e.target.innerHTML = FULL_HEART
+        }
+        // activated 
+        // ? e.target.classList.remove("activated-heart") 
+        // : e.target.classList.add("activated-heart")
+      })
+      .catch(error => {
+        console.log(error);
+        errorModal.remove("hidden");
+        setTimeout(() => {
+          hideError()
+        }, 3000)
+      })
+    }
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
